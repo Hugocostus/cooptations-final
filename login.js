ab6ed8e1fbcb1eb2d524dda52a0f36c8d731a75b
Hugocostus <costus18@gmail.com>
2025-11-25 19:10:30 +0100

    Rename variables for email and number in login.js

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 const AUTH_API = "https://script.google.com/macros/s/AKfycbx-BjQwlAJMZhRdBZKPtzFJBfLbB5GiCV9fKNDQnG94YhvVHH8DcCr5t0kQwz0PcIq7tw/exec";
 
 document.getElementById("login-btn").addEventListener("click", async () => {
-    const email = document.getElementById("email").value.trim();
-    const numero = document.getElementById("numero").value.trim();
+    const Adresse = document.getElementById("email").value.trim();
+    const Numero = document.getElementById("numero").value.trim();
     const status = document.getElementById("login-status");
 
-    if (!email || !numero) {
+    if (!Adresse || !Numero) {
         status.textContent = "Veuillez remplir les deux champs.";
         return;
     }
