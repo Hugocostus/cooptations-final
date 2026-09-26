5d867c1404fe81d3fdd308e21b86343f7740f529
Hugocostus <costus18@gmail.com>
2026-09-26 17:24:00 +0200

    Update API URL and maintain student functionality
    Updated API_URL and ensured the student loading and submission functionality remains intact.

--- script asso.js
+++ script asso.js
@@ -1,5 +1,5 @@
 // === Global Variables ===
-const API_URL = "https://script.google.com/macros/s/AKfycbw6yfWJ26quYCU0AQcXjRgpUdZ837-NUW26RqsQddvaZE_BYUf4583SKaSZ-jD3yQ1sKA/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzYyvdkRqcRNj9xWWC0Gks1pq9GZbSzaHb1IsLzgF5gWoPP99owK7xCDrprP8D5CXVHKQ/exec";
 let etudiants = [];
 
 // 🔥 Keep selected students here
