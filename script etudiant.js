197b2a5ee36b4bf00d1555bf88e88836aa0e4502
Hugocostus <costus18@gmail.com>
2026-09-26 17:41:03 +0200

    Fix formatting issues and update API URL

--- script etudiant.js
+++ script etudiant.js
@@ -2,7 +2,7 @@
           ⚙️ GLOBAL VARIABLES
    ================================ */
 
-const API_URL = "https://AKfycbzYyvdkRqcRNj9xWWC0Gks1pq9GZbSzaHb1IsLzgF5gWoPP99owK7xCDrprP8D5CXVHKQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzYyvdkRqcRNj9xWWC0Gks1pq9GZbSzaHb1IsLzgF5gWoPP99owK7xCDrprP8D5CXVHKQ/exec";
 
 let etudiantConnecte = null; // Will contain the object {Prenom, Nom, Numero}
 
