78743e288b86bbd94ef8197a9f5c31866bd9f0ee
Hugocostus <costus18@gmail.com>
2026-09-26 17:23:46 +0200

    Update API URL and clean up code structure

--- script etudiant.js
+++ script etudiant.js
@@ -2,7 +2,7 @@
           ⚙️ GLOBAL VARIABLES
    ================================ */
 
-const API_URL = "https://script.google.com/macros/s/AKfycbw6yfWJ26quYCU0AQcXjRgpUdZ837-NUW26RqsQddvaZE_BYUf4583SKaSZ-jD3yQ1sKA/exec";
+const API_URL = "https://AKfycbzYyvdkRqcRNj9xWWC0Gks1pq9GZbSzaHb1IsLzgF5gWoPP99owK7xCDrprP8D5CXVHKQ/exec";
 
 let etudiantConnecte = null; // Will contain the object {Prenom, Nom, Numero}
 
